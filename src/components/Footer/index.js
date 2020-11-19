8a5be5c83fa2c57093ad0be4b1be4bf0dcf2b0d7
Stuart Wong <swong31@gmail.com>
2020-11-18 21:06:10 -0800

    Created About, Portfolio and Contact Pages; created Col, Container, Footer, Hero, Navbar, Project, Row and Wrapper components

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <span>Stuart Wong 2020</span>
+    </footer>
+  );
+}
+
+export default Footer;
